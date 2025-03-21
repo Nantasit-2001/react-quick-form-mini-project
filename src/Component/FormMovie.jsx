6fbb6fdddedb50c5c8769be6a609821529d551cb
nantasit <nantasitnsboss@gmail.com>
2025-03-21 16:11:59 +0700

    refactor:create component-FormSusseed and use instead of the old ones in FormMovie

--- src/Component/FormMovie.jsx
+++ src/Component/FormMovie.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-import { Film,RotateCcw,Send,CircleCheckBig    } from 'lucide-react';
+import { Film,RotateCcw,Send,} from 'lucide-react';
 import { movies } from "../Data/movise";
-  
+import SurveyResult from "./FormSucceed";
 function FormMovie () {
     const [selectedOption, setSelectedOption] = useState("");
     const [textInput, setTextInput] = useState({});
     const [dataError,setDataError] = useState({});
-    const [formSucceed,setFormSucceed] = useState(false)
+    const [formSucceedInput,setFormSucceedInput] = useState(false)
 
     const BackToForm=()=>{
         resetValue();
-        setFormSucceed(false);
+        setFormSucceedInput(false);
     }
     const ThenSubmit = (event) => {
         event.preventDefault();
         setDataError({})
-        CheckCorrectValue()?setFormSucceed(true):console.log("❌ => ", dataError)
+        CheckCorrectValue()?setFormSucceedInput(true):console.log("❌ => ", dataError)
     };
     const CheckCorrectValue = () =>{
         let tempError={}
@@ -50,81 +50,57 @@ function FormMovie () {
         </>
 )       
     }
-    function RenderShowDataInput(topicName,detaInTopic,className){
-        return(
-            <div className="grid grid-cols-3 gap-1">
-                        <p className="text-sm font-medium text-gray-500">{topicName}</p>
-                        <p className={`${className}`}>{detaInTopic}</p>
-            </div>
-        )
-    }
     return(
         <>
         <section className="w-full max-w-md shadow- mx-2">
             <div className="flex flex-col space-y-1.5 p-6 bg-gradient-to-r from-purple-700 to-indigo-600 text-white" >
                 <h1 className="font-semibold tracking-tight flex items-center gap-2 text-2xl"> <Film/>Movie Survey</h1>
             </div>
-{formSucceed?
-        <section className="bg-white p-6">
-            <div className="rounded-lg bg-green-50 p-4 border border-green-200">
-                <h2 className="text-xl text-green-800 flex items-center gap-2 mb-4"><CircleCheckBig/>ส่งแบบสำรวจสำเร็จ!</h2>
-                <div className="space-y-3">
+            {formSucceedInput?
+                (<SurveyResult textInput={textInput} selectedOption={selectedOption} BackToForm={BackToForm} />)
+                : // form
+                <section>
+                <form onSubmit={ThenSubmit} className="flex flex-col p-8 bg-white">
+                    {RenderLableAndInput("name","text","ชื่อ","กรุณากรอกชื่อของคุณ")}
+                    {RenderLableAndInput("email","email","อีเมล","example@email.com")}
 
-                    {RenderShowDataInput("ชื่อ:",textInput.name,"text-sm")}
-                    {RenderShowDataInput("อีเมล:",textInput.email,"text-sm")}
-                    {RenderShowDataInput("หนังที่เลือก:",selectedOption,"text-sm font-medium text-purple-700 ")}
-                    <div className="mt-4 pt-4 border-t border-gray-300">
-                        <p className="text-sm font-medium text-gray-500 mb-2">ความคิดเห็น</p>
-                        <h3 className="text-sm bg-gray-50 p-3 rounded-md">{textInput.director}</h3>
+                    <h2 className="mt-4 mb-3">เลือกหนังที่คุณชอบ<RenderAsteriskRed/></h2>
+                    <div className={`flex flex-col justify-around h-120 px-3 mb-2 ${"selectedOption" in dataError?`border rounded-md border-red-500`:null}`}>
+                            {movies.map((movie) => (
+                                <label key={movie.title} className="flex items-center gap-2 p-2 rounded-md cursor-pointer transition-colors duration-200 hover:bg-blue-50">
+                                    <input
+                                        type="radio"
+                                        name="option"
+                                        value={movie.title}
+                                        checked={selectedOption === movie.title}
+                                        onChange={(event) => setSelectedOption(event.target.value)}
+                                        className="mt-0.5"
+                                    />
+                                    <div>
+                                        <span className="font-medium">{`${movie.title} (${movie.year})`}</span>
+                                        <span className="block text-gray-500">{`Director: ${movie.director}`}</span>
+                                    </div>
+                                </label>
+                            ))}
                     </div>
-                </div>            
-            </div>
-            <button className=" cursor-pointer rounded-md text-sm text-white h-10 px-4 py-2 w-full flex items-center justify-center gap-2 mt-4 bg-black" 
-            onClick={BackToForm}> <RotateCcw size={18}/>ทำแบบสำรวจใหม่</button>
-        </section>
-: // form
-        <section>
-            <form onSubmit={ThenSubmit} className="flex flex-col p-8 bg-white">
-                {RenderLableAndInput("name","text","ชื่อ","กรุณากรอกชื่อของคุณ")}
-                {RenderLableAndInput("email","email","อีเมล","example@email.com")}
+                    {"selectedOption" in dataError?<span className="text-sm text-red-500">{dataError.selectedOption}</span>:null}
 
-                <h2 className="mt-4 mb-3">เลือกหนังที่คุณชอบ<RenderAsteriskRed/></h2>
-                <div className={`flex flex-col justify-around h-120 px-3 mb-2 ${"selectedOption" in dataError?`border rounded-md border-red-500`:null}`}>
-                        {movies.map((movie) => (
-                            <label key={movie.title} className="flex items-center gap-2 p-2 rounded-md cursor-pointer transition-colors duration-200 hover:bg-blue-50">
-                                <input
-                                    type="radio"
-                                    name="option"
-                                    value={movie.title}
-                                    checked={selectedOption === movie.title}
-                                    onChange={(event) => setSelectedOption(event.target.value)}
-                                    className="mt-0.5"
-                                />
-                                <div>
-                                    <span className="font-medium">{`${movie.title} (${movie.year})`}</span>
-                                    <span className="block text-gray-500">{`Director: ${movie.director}`}</span>
-                                </div>
-                            </label>
-                        ))}
-                </div>
-                {"selectedOption" in dataError?<span className="text-sm text-red-500">{dataError.selectedOption}</span>:null}
-
-                <label htmlFor="comment" className="mt-6 mb-2">ความคิดเห็นเกี่ยวกับหนัง</label>
-                <textarea className="rounded-md border border-gray-300 p-2 " rows="4"placeholder="พิมพ์ความคิดเห็นของคุณที่นี่..."
-                    value={textInput.director || ""}
-                    onChange={(event)=>setTextInput({ ...textInput, director: event.target.value })}
-                ></textarea>
+                    <label htmlFor="comment" className="mt-6 mb-2">ความคิดเห็นเกี่ยวกับหนัง</label>
+                    <textarea className="rounded-md border border-gray-300 p-2 " rows="4"placeholder="พิมพ์ความคิดเห็นของคุณที่นี่..."
+                        value={textInput.director || ""}
+                        onChange={(event)=>setTextInput({ ...textInput, director: event.target.value })}
+                    ></textarea>
                 
-                <hr className="text-gray-300 my-6"/>
+                    <hr className="text-gray-300 my-6"/>
                 
-                <div className="flex flex-row justify-between">
-                    <button className="cursor-pointer rounded-md border text-sm border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex items-center gap-1"
-                                        type="button" onClick={resetValue} ><RotateCcw size={15} />รีเซ็ต</button> 
-                    <button className="cursor-pointer rounded-md text-sm font-medium h-10 px-4 py-2 bg-gradient-to-r from-purple-700 to-indigo-600 text-white flex items-center gap-1"
-                    type="submit"><Send size={15}/>ส่งแบบสำรวจ</button>
-                </div>
-            </form>
-        </section>
+                    <div className="flex flex-row justify-between">
+                        <button className="cursor-pointer rounded-md border text-sm border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex items-center gap-1"
+                                            type="button" onClick={resetValue} ><RotateCcw size={15} />รีเซ็ต</button> 
+                        <button className="cursor-pointer rounded-md text-sm font-medium h-10 px-4 py-2 bg-gradient-to-r from-purple-700 to-indigo-600 text-white flex items-center gap-1"
+                        type="submit"><Send size={15}/>ส่งแบบสำรวจ</button>
+                    </div>
+                </form> 
+                </section>
     }
     </section>
         </>
